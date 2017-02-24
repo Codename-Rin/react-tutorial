0121283f252f3e6639c66c8f6e014fea28ba1f8f
Mateusz Folga <mateusz.folga@chop-chop.org>
2017-02-25 00:45:11 +0100

    react part 3

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>It's working!</h1>
-				<p>As intended.</p>
-			</div>
-		);
-	}
-}
+import Layout from './components/Layout';
 
 const app = document.getElementById('app');
-
 ReactDOM.render(<Layout/>, app);
